4c4c22b49197415e97223fbdebbfa0e64361198b
Tien Manh <nguyentienmanh6@gmail.com>
2019-01-11 23:28:02 +0700

    completed code

--- js/all.js
+++ js/all.js
@@ -3,13 +3,7 @@ var sidebar = {
 }
 
 window.addEventListener('load', function(ev){
-    var iconplus = document.getElementsByClassName('sidebar-icon-plus');
-
-    for(var i = 0; i < iconplus.length; ++i){
-        iconplus[i].onclick = function(){
-            expandSideMenu(this.id);
-        }
-    }
+    setActionIconPlus();
 
     document.getElementById('icon-expand').onclick = function(){
         document.getElementById('sidebar').style.transform = 'translateX(0)';
@@ -25,26 +19,31 @@ window.addEventListener('load', function(ev){
 
 function expandSideMenu(des){
     if(sidebar.current !== 0) {
-        var current = document.getElementById(sidebar.current + '-sidebar-class-subject');
-        current.style.height = 0;
         document.getElementById(sidebar.current).classList.remove('fa-minus-circle');
         document.getElementById(sidebar.current).classList.add('fa-plus-circle');
+        document.getElementById(sidebar.current + '-sidebar-class-subject').style.height = 0;
+        setActionIconPlus();
     }
 
+    document.getElementById(des).classList.remove('fa-plus-circle');
+    document.getElementById(des).classList.add('fa-minus-circle');
     document.getElementById(des + '-sidebar-class-subject').style.height = ( document.getElementById(des + '-sidebar-class-subject').childElementCount * 37 - 2 ) + 'px';
-    var desElement = document.getElementById(des);
-    desElement.classList.remove('fa-plus-circle');
-    desElement.classList.add('fa-minus-circle');
-
-    desElement.addEventListener('click', function(){
-        desElement.classList.remove('fa-minus-circle');
-        desElement.classList.add('fa-plus-circle');
+    document.getElementById(des).onclick = function(){
         document.getElementById(des + '-sidebar-class-subject').style.height = 0;
-        sidebar.current = 0;
-        document.getElementById(des).addEventListener('click', function(){
-            expandSideMenu(des);
-        });
-    })
+        document.getElementById(des).classList.remove('fa-minus-circle');
+        document.getElementById(des).classList.add('fa-plus-circle');
+        setActionIconPlus();
+    }
 
     sidebar.current = des;
+}
+
+function setActionIconPlus(){
+    var iconplus = document.getElementsByClassName('sidebar-icon-plus');
+
+    for(var i = 0; i < iconplus.length; ++i){
+        iconplus[i].onclick = function(){
+            expandSideMenu(this.id);
+        }
+    }
 }
