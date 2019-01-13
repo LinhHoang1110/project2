197a7a3530133b09636a5ba966feff80145a7eb9
Tien Manh <nguyentienmanh6@gmail.com>
2019-01-13 22:41:33 +0700

    sửa các yêu cầu trong góp ý lần 1

--- js/all.js
+++ js/all.js
@@ -2,24 +2,47 @@ var sidebar = {
     current: 0
 }
 
+var scroll_top = false;
+
 window.addEventListener('load', function(ev){
     setActionIconPlus();
 
     document.getElementById('icon-expand').onclick = function(){
         document.getElementById('sidebar').style.transform = 'translateX(0)';
         document.getElementById('sidebar-blur').style.transform = 'translateX(0)';
+        document.getElementsByTagName('body')[0].style.overflow = 'hidden';
         document.getElementById('icon-close').onclick = function(){
             document.getElementById('sidebar').style.transform = 'translateX(-100%)';
             document.getElementById('sidebar-blur').style.transform = 'translateX(-100%)';
+            document.getElementsByTagName('body')[0].style.overflow = 'unset';
         }
     }
 
     // document.getElementById('header-input').style.width = 150px;
+    document.getElementsByClassName('scroll-top')[0].addEventListener('click',function(){
+        // document.body.scrollTop = 0;
+        // document.documentElement.scrollTop = 0;
+        scrollToTop(500);
+    })
 })
 window.addEventListener('resize', function(){
     if(document.body.clientWidth > 732) {
         document.getElementById('sidebar').style.transform = 'translateX(-100%)';
         document.getElementById('sidebar-blur').style.transform = 'translateX(-100%)';
+        document.getElementsByTagName('body')[0].style.overflow = 'unset';
+    }
+})
+
+window.addEventListener('scroll', function(){
+    console.log(window.pageYOffset);
+    if(window.pageYOffset != 0) {
+        if(!scroll_top) {
+            document.getElementsByClassName('scroll-top')[0].style.opacity = 0.3;
+            scroll_top = true;
+        }
+    } else {
+        document.getElementsByClassName('scroll-top')[0].style.opacity = 0;
+        scroll_top = false;
     }
 })
 
@@ -52,4 +75,18 @@ function setActionIconPlus(){
             expandSideMenu(this.id);
         }
     }
+}
+
+/**
+ * function to scroll top
+ * @param {ms} scrollDuration 
+ */
+function scrollToTop(scrollDuration) {
+    var scrollStep = -window.scrollY / (scrollDuration / 15),
+        scrollInterval = setInterval(function(){
+        if ( window.scrollY != 0 ) {
+            window.scrollBy( 0, scrollStep );
+        }
+        else clearInterval(scrollInterval); 
+    },15);
 }
