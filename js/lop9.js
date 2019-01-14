2b26c1553f34b9da8efbe6e502b6abfbdcb2cf09
Tien Manh <nguyentienmanh6@gmail.com>
2019-01-14 11:26:12 +0700

    them preload

--- js/lop9.js
+++ js/lop9.js
@@ -1,82 +1,96 @@
 var sv = ''
-+ '<div class="grade-post">'
-+ '<div class="post-title">Phân tích đấu tranh cho một thế giới hòa bình</div>'
-+ '<div class="post-author">Nguyễn Công Hoan</div>'
-+ '<div class="post-statistic">'
-+ '    <div class="post-like"><i class="fas fa-heart" style="color: #e06666; margin-right: 6px;"></i>96</div>'
-+ '    <div class="post-view"><i class="far fa-eye" style="color: #6ea3ed; margin-right: 6px;"></i>342</div>'
-+ '</div>'
-+ '<div class="post-content">Có xu hướng đe dọa các đồng minh lâu năm, nhưng lại gần gũi với kẻ thù và cự tuyệt mọi lời tư vấn có xu hướng công khai đe dọa các đồng minh lâu năm</div>'
-+ '</div>'
-+ '<div class="grade-post">'
-+ '<div class="post-title">Phân tích đấu tranh cho một thế giới hòa bình</div>'
-+ '<div class="post-author">Nguyễn Công Hoan</div>'
-+ '<div class="post-statistic">'
-+ '    <div class="post-like"><i class="fas fa-heart" style="color: #e06666; margin-right: 6px;"></i>96</div>'
-+ '    <div class="post-view"><i class="far fa-eye" style="color: #6ea3ed; margin-right: 6px;"></i>342</div>'
-+ '</div>'
-+ '<div class="post-content">Có xu hướng đe dọa các đồng minh lâu năm, nhưng lại gần gũi với kẻ thù và cự tuyệt mọi lời tư vấn có xu hướng công khai đe dọa các đồng minh lâu năm</div>'
-+ '</div>'
-+ '<div class="grade-post">'
-+ '<div class="post-title">Phân tích đấu tranh cho một thế giới hòa bình</div>'
-+ '<div class="post-author">Nguyễn Công Hoan</div>'
-+ '<div class="post-statistic">'
-+ '    <div class="post-like"><i class="fas fa-heart" style="color: #e06666; margin-right: 6px;"></i>96</div>'
-+ '    <div class="post-view"><i class="far fa-eye" style="color: #6ea3ed; margin-right: 6px;"></i>342</div>'
-+ '</div>'
-+ '<div class="post-content">Có xu hướng đe dọa các đồng minh lâu năm, nhưng lại gần gũi với kẻ thù và cự tuyệt mọi lời tư vấn có xu hướng công khai đe dọa các đồng minh lâu năm</div>'
-+ '</div>'
-+ '<div class="grade-post">'
-+ '<div class="post-title">Phân tích đấu tranh cho một thế giới hòa bình</div>'
-+ '<div class="post-author">Nguyễn Công Hoan</div>'
-+ '<div class="post-statistic">'
-+ '    <div class="post-like"><i class="fas fa-heart" style="color: #e06666; margin-right: 6px;"></i>96</div>'
-+ '    <div class="post-view"><i class="far fa-eye" style="color: #6ea3ed; margin-right: 6px;"></i>342</div>'
-+ '</div>'
-+ '<div class="post-content">Có xu hướng đe dọa các đồng minh lâu năm, nhưng lại gần gũi với kẻ thù và cự tuyệt mọi lời tư vấn có xu hướng công khai đe dọa các đồng minh lâu năm</div>'
-+ '</div>'
-+ '<div class="grade-post">'
-+ '<div class="post-title">Phân tích đấu tranh cho một thế giới hòa bình</div>'
-+ '<div class="post-author">Nguyễn Công Hoan</div>'
-+ '<div class="post-statistic">'
-+ '    <div class="post-like"><i class="fas fa-heart" style="color: #e06666; margin-right: 6px;"></i>96</div>'
-+ '    <div class="post-view"><i class="far fa-eye" style="color: #6ea3ed; margin-right: 6px;"></i>342</div>'
-+ '</div>'
-+ '<div class="post-content">Có xu hướng đe dọa các đồng minh lâu năm, nhưng lại gần gũi với kẻ thù và cự tuyệt mọi lời tư vấn có xu hướng công khai đe dọa các đồng minh lâu năm</div>'
-+ '</div>'
-+ '<div class="grade-post">'
-+ '<div class="post-title">Phân tích đấu tranh cho một thế giới hòa bình</div>'
-+ '<div class="post-author">Nguyễn Công Hoan</div>'
-+ '<div class="post-statistic">'
-+ '    <div class="post-like"><i class="fas fa-heart" style="color: #e06666; margin-right: 6px;"></i>96</div>'
-+ '    <div class="post-view"><i class="far fa-eye" style="color: #6ea3ed; margin-right: 6px;"></i>342</div>'
-+ '</div>'
-+ '<div class="post-content">Có xu hướng đe dọa các đồng minh lâu năm, nhưng lại gần gũi với kẻ thù và cự tuyệt mọi lời tư vấn có xu hướng công khai đe dọa các đồng minh lâu năm</div>'
-+ '</div>'
-+ '<div class="grade-post">'
-+ '<div class="post-title">Phân tích đấu tranh cho một thế giới hòa bình</div>'
-+ '<div class="post-author">Nguyễn Công Hoan</div>'
-+ '<div class="post-statistic">'
-+ '    <div class="post-like"><i class="fas fa-heart" style="color: #e06666; margin-right: 6px;"></i>96</div>'
-+ '    <div class="post-view"><i class="far fa-eye" style="color: #6ea3ed; margin-right: 6px;"></i>342</div>'
-+ '</div>'
-+ '<div class="post-content">Có xu hướng đe dọa các đồng minh lâu năm, nhưng lại gần gũi với kẻ thù và cự tuyệt mọi lời tư vấn có xu hướng công khai đe dọa các đồng minh lâu năm</div>'
-+ '</div>';
+    + '<div class="grade-post grade-post-invisible">'
+    + '<div class="post-title">Phân tích đấu tranh cho một thế giới hòa bình</div>'
+    + '<div class="post-author">Nguyễn Công Hoan</div>'
+    + '<div class="post-statistic">'
+    + '    <div class="post-like"><i class="fas fa-heart" style="color: #e06666; margin-right: 6px;"></i>96</div>'
+    + '    <div class="post-view"><i class="far fa-eye" style="color: #6ea3ed; margin-right: 6px;"></i>342</div>'
+    + '</div>'
+    + '<div class="post-content">Có xu hướng đe dọa các đồng minh lâu năm, nhưng lại gần gũi với kẻ thù và cự tuyệt mọi lời tư vấn có xu hướng công khai đe dọa các đồng minh lâu năm</div>'
+    + '</div>'
+    + '<div class="grade-post grade-post-invisible">'
+    + '<div class="post-title">Phân tích đấu tranh cho một thế giới hòa bình</div>'
+    + '<div class="post-author">Nguyễn Công Hoan</div>'
+    + '<div class="post-statistic">'
+    + '    <div class="post-like"><i class="fas fa-heart" style="color: #e06666; margin-right: 6px;"></i>96</div>'
+    + '    <div class="post-view"><i class="far fa-eye" style="color: #6ea3ed; margin-right: 6px;"></i>342</div>'
+    + '</div>'
+    + '<div class="post-content">Có xu hướng đe dọa các đồng minh lâu năm, nhưng lại gần gũi với kẻ thù và cự tuyệt mọi lời tư vấn có xu hướng công khai đe dọa các đồng minh lâu năm</div>'
+    + '</div>'
+    + '<div class="grade-post grade-post-invisible">'
+    + '<div class="post-title">Phân tích đấu tranh cho một thế giới hòa bình</div>'
+    + '<div class="post-author">Nguyễn Công Hoan</div>'
+    + '<div class="post-statistic">'
+    + '    <div class="post-like"><i class="fas fa-heart" style="color: #e06666; margin-right: 6px;"></i>96</div>'
+    + '    <div class="post-view"><i class="far fa-eye" style="color: #6ea3ed; margin-right: 6px;"></i>342</div>'
+    + '</div>'
+    + '<div class="post-content">Có xu hướng đe dọa các đồng minh lâu năm, nhưng lại gần gũi với kẻ thù và cự tuyệt mọi lời tư vấn có xu hướng công khai đe dọa các đồng minh lâu năm</div>'
+    + '</div>'
+    + '<div class="grade-post grade-post-invisible">'
+    + '<div class="post-title">Phân tích đấu tranh cho một thế giới hòa bình</div>'
+    + '<div class="post-author">Nguyễn Công Hoan</div>'
+    + '<div class="post-statistic">'
+    + '    <div class="post-like"><i class="fas fa-heart" style="color: #e06666; margin-right: 6px;"></i>96</div>'
+    + '    <div class="post-view"><i class="far fa-eye" style="color: #6ea3ed; margin-right: 6px;"></i>342</div>'
+    + '</div>'
+    + '<div class="post-content">Có xu hướng đe dọa các đồng minh lâu năm, nhưng lại gần gũi với kẻ thù và cự tuyệt mọi lời tư vấn có xu hướng công khai đe dọa các đồng minh lâu năm</div>'
+    + '</div>'
+    + '<div class="grade-post grade-post-invisible">'
+    + '<div class="post-title">Phân tích đấu tranh cho một thế giới hòa bình</div>'
+    + '<div class="post-author">Nguyễn Công Hoan</div>'
+    + '<div class="post-statistic">'
+    + '    <div class="post-like"><i class="fas fa-heart" style="color: #e06666; margin-right: 6px;"></i>96</div>'
+    + '    <div class="post-view"><i class="far fa-eye" style="color: #6ea3ed; margin-right: 6px;"></i>342</div>'
+    + '</div>'
+    + '<div class="post-content">Có xu hướng đe dọa các đồng minh lâu năm, nhưng lại gần gũi với kẻ thù và cự tuyệt mọi lời tư vấn có xu hướng công khai đe dọa các đồng minh lâu năm</div>'
+    + '</div>'
+    + '<div class="grade-post grade-post-invisible">'
+    + '<div class="post-title">Phân tích đấu tranh cho một thế giới hòa bình</div>'
+    + '<div class="post-author">Nguyễn Công Hoan</div>'
+    + '<div class="post-statistic">'
+    + '    <div class="post-like"><i class="fas fa-heart" style="color: #e06666; margin-right: 6px;"></i>96</div>'
+    + '    <div class="post-view"><i class="far fa-eye" style="color: #6ea3ed; margin-right: 6px;"></i>342</div>'
+    + '</div>'
+    + '<div class="post-content">Có xu hướng đe dọa các đồng minh lâu năm, nhưng lại gần gũi với kẻ thù và cự tuyệt mọi lời tư vấn có xu hướng công khai đe dọa các đồng minh lâu năm</div>'
+    + '</div>'
+    + '<div class="grade-post grade-post-invisible">'
+    + '<div class="post-title">Phân tích đấu tranh cho một thế giới hòa bình</div>'
+    + '<div class="post-author">Nguyễn Công Hoan</div>'
+    + '<div class="post-statistic">'
+    + '    <div class="post-like"><i class="fas fa-heart" style="color: #e06666; margin-right: 6px;"></i>96</div>'
+    + '    <div class="post-view"><i class="far fa-eye" style="color: #6ea3ed; margin-right: 6px;"></i>342</div>'
+    + '</div>'
+    + '<div class="post-content">Có xu hướng đe dọa các đồng minh lâu năm, nhưng lại gần gũi với kẻ thù và cự tuyệt mọi lời tư vấn có xu hướng công khai đe dọa các đồng minh lâu năm</div>'
+    + '</div>';
 
-window.addEventListener('load', function(){
-    document.getElementById('expand-sv').addEventListener('click', function(){
-        document.getElementsByClassName('blur-content')[0].classList.remove('blur-content');
-        var news_content = document.getElementById('sv-grade-news-content');
-        news_content.style.height = news_content.style.height;
-        news_content.innerHTML += sv;
-        news_content.style.height = 'auto';
-        document.getElementById('th').style.display = 'none';
+window.addEventListener('load', function () {
+    document.getElementById('expand-sv').addEventListener('click', function () {
+        document.getElementById('preload-icon').style.opacity = 1;
+        document.getElementById('preload-icon').style.width = '32px';
 
         this.style.display = 'none';
 
-        var grade_circle = document.getElementsByClassName('grade-circle');
-        for(var i = 0; i < grade_circle.length; ++i){
-            grade_circle[i].style.display = 'block';
-        }
+        setTimeout(function () {
+            document.getElementById('preload-icon').style.opacity = 0;
+            document.getElementsByClassName('blur-content')[0].classList.remove('blur-content');
+            var news_content = document.getElementById('sv-grade-news-content');
+            news_content.innerHTML += sv;
+            document.getElementById('th').style.display = 'none';
+
+            var grade_circle = document.getElementsByClassName('grade-circle');
+            for (var i = 0; i < grade_circle.length; ++i) {
+                grade_circle[i].style.display = 'block';
+            }
+        }, 2000)
+
+
+        setTimeout(() => {
+            var post_invisible = document.getElementsByClassName('grade-post-invisible');
+
+            for (var i = 0; i < post_invisible.length; ++i) {
+                post_invisible[i].style.opacity = 1;
+                post_invisible[i].style.transform = 'translateY(0px)';
+            }
+        }, 2200);
     })
 });
