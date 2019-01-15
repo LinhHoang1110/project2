c8ff25ded0f4905763ce7c19257766ff73dc1d8f
Tien Manh <nguyentienmanh6@gmail.com>
2019-01-15 17:26:49 +0700

    test pagespeed comment facebook

--- js/detail.js
+++ js/detail.js
@@ -8,7 +8,28 @@ function loadAPI() {
 
 window.addEventListener('scroll', function () {
     if (cmtFb) {
-        loadAPI;
+        loadAPI();
         cmtFb = false;
     }
 })
+
+// var cmtFb = true;
+
+// function loadAPI() {
+//     var d = document;
+//     var s = 'script';
+//     var id = 'facebook-jssdk';
+//     var js, fjs = d.getElementsByTagName(s)[0];
+//     if (d.getElementById(id)) return;
+//     js = d.createElement(s); js.id = id;
+//     js.async = true;
+//     js.src = 'https://connect.facebook.net/en_GB/sdk.js#xfbml=1&version=v3.2';
+//     fjs.parentNode.insertBefore(js, fjs);
+// }
+
+// window.addEventListener('scroll', function () {
+//     if (cmtFb) {
+//         loadAPI();
+//         cmtFb = false;
+//     }
+// })
